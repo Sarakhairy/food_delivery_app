8c102763b7a1bcda06d9bfe54951305438e11e71
SaraKhairy <sarahkhairy180@gmail.com>
2025-11-03 15:54:06 +0200

    finishing splash screen

--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/core/constants/app_colors.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(backgroundColor: AppColors.primaryColor,body: Center(
+      child: Image.asset('assets/images/logo.png'),
+    ),);
   }
 }
